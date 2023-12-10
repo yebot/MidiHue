e70475dab8ab044ff01d6dbca0971f1991175308
Tobey Forsman <tobeyforsman@gmail.com>
2023-12-10 13:38:29 -0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /Users/goldenboy/Code/Projects/MidiHue/venv/bin/activate
+/Users/goldenboy/Code/Projects/MidiHue/venv/bin/python3 -u /Users/goldenboy/Code/Projects/MidiHue/main.py
+deactivate
